2ebec19facc686bf7c7bfce98cf939dde8c1a8d8
stevenalston <salston@eliteclientele.com>
2021-06-09 12:45:23 -0500

    adding application

--- db_persistence.rb
+++ db_persistence.rb
@@ -16,25 +16,37 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+      SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(todos.is_completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON lists.id = todos.list_id
+        WHERE lists.id = $1
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
+
     result = query(sql, id)
-    tuple = result.first
 
-    list_id = tuple["id"].to_i
-    todos = find_todos(list_id)
-
-    {id: list_id, name: tuple["name"], todos: todos}
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
-    sql = "SELECT * FROM lists"
+    sql = <<~SQL
+      SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(todos.is_completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON lists.id = todos.list_id
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
+
     result = query(sql)
 
     result.map do |tuple|
-      list_id = tuple["id"].to_i
-      todos = find_todos(list_id)
-
-      { id: list_id, name: tuple["name"], todos: todos }
+      tuple_to_list_hash(tuple)
     end
   end
 
@@ -82,11 +94,18 @@ class DatabasePersistence
     @db.close
   end
 
-  private
   def find_todos(list_id)
     todo_sql = "SELECT * FROM todos WHERE list_id = $1"
     todos_result = query(todo_sql, list_id)
 
     todos_result.map { |t| {id: t["id"].to_i, name: t["name"], is_completed: t["is_completed"] == "t" }}
+  end
+
+  private
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 end
--- todo.rb
+++ todo.rb
@@ -17,19 +17,11 @@ end
 
 helpers do
   def list_complete?(list)
-    todos_count(list) > 0 && todos_remaining_count(list) == 0
+    list[:todos_count] > 0 && list[:todos_remaining_count] == 0
   end
 
   def list_class(list)
     "complete" if list_complete?(list)
-  end
-
-  def todos_count(list)
-    list[:todos].size
-  end
-
-  def todos_remaining_count(list)
-    list[:todos].count { |todo| !todo[:is_completed] }
   end
 
   def sort_lists(lists, &block)
@@ -109,6 +101,7 @@ end
 get "/lists/:id" do
   @list_id = params[:id].to_i
   @list = load_list(@list_id)
+  @todos = @storage.find_todos(@list_id)
   erb :list, layout: :layout
 end
 
